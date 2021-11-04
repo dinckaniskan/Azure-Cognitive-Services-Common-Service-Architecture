ef3eaf2d7c6b8171237c328c7734d42cae13e172
Stuart Eggerton <gitstua@users.noreply.github.com>
2021-11-04 23:30:12 +1100

    Update deploy.sh

--- bicep/deploy.sh
+++ bicep/deploy.sh
@@ -1,3 +1,3 @@
-rgName=xxcogservices2
+rgName=xxcogservices2 
 az group create -n $rgName -l australiaeast
 az deployment group create --template-file ./main.bicep --resource-group $rgName --parameters environmentType=nonprod
